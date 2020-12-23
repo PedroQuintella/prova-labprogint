983d22e024469389da3afa2b7cae4999268557b0
Pedro Quintella <pedro.q2000@outlook.com>
2020-12-23 20:07:07 -0300

    Feat: Criação da página index.html, do style.css e do index_controller
    Foi criada a página index.html, seu arquivo de estilização style.css e seu controller.

---
+++ application/controller/index_controller.py
@@ -0,0 +1,11 @@
+from flask import render_template, request
+from application import app
+from application.model.entity.produto import Produto
+from application.model.dao.produto_dao import ProdutoDAO
+
+
+@app.route("/")
+def index():
+    listaProdutos = ProdutoDAO().get_listar_produtos()
+    return render_template('index.html', listaProdutos = listaProdutos)
+
