4fe973baefd6abeb5e5d6c033522f195609383bd
Pedro Quintella <pedro.q2000@outlook.com>
2020-12-23 19:57:23 -0300

    Fix: Correção do produto_dao.py e do __init__.py
    Foi corrigido o produto_dao.py, para adição do json e remoção das funções listar_produtos() e buscar_por_id(). E removidos os produtos cadastrados anteriormente no __init__.py.

--- application/__init__.py
+++ application/__init__.py
@@ -4,12 +4,4 @@ from application.model.entity.produto import Produto
 
 app = Flask(__name__, static_folder=os.path.abspath("application/view/static"), template_folder=os.path.abspath("application/view/templates"))
 
-produto1 = Produto(1, "Nome do Produto", "Descrição do produto um pouco maior, com duas linhas ou três que explica melhor do que se trata.", "23,99", "19,99", "2x de R$9,99")
-produto2 = Produto(2, "Nome do Produto", "Descrição do produto um pouco maior, com duas linhas ou três que explica melhor do que se trata.", "23,99", "19,99", "2x de R$9,99")
-produto3 = Produto(3, "Nome do Produto", "Descrição do produto um pouco maior, com duas linhas ou três que explica melhor do que se trata.", "23,99", "19,99", "2x de R$9,99")
-produto4 = Produto(4, "Nome do Produto", "Descrição do produto um pouco maior, com duas linhas ou três que explica melhor do que se trata.", "23,99", "19,99", "2x de R$9,99")
-
-listaProdutos = [produto1, produto2, produto3, produto4]
-
-
 from application.controller import index_controller
--- application/model/dao/produto_dao.py
+++ application/model/dao/produto_dao.py
@@ -1,16 +1,16 @@
 from application.model.entity.produto import Produto
-from application import listaProdutos
+import json
 
 class ProdutoDAO:
 
     def __init__(self):
-        self._listaProdutos = listaProdutos
+        self._listaProdutos = []
     
-    def listar_produtos(self):
+        with open('C:\\Users\\pedro\\Documents\\Arquivos Pedro\\Arquivos da Faculdade\\Laboratório de Programação de Interface com o Usuário\\Prova P2\\prova-labprogint\\application\\view\\static\\json\\products.json') as product_file:
+            product_list = json.load(product_file)
+    
+        for product in product_list:
+            self._listaProdutos.append(Produto(product['id'],product['name'], product['image'], product['description'], product['oldPrice'], product['price'], product['installments']['value'], product['installments']['count']))
+
+    def get_listar_produtos(self):
         return self._listaProdutos
-    
-    def buscar_por_id(self, id):
-        for produto in range(0, len(self._listaProdutos)):
-            if self._listaProdutos[produto].get_id() == int(id):
-                return self._listaProdutos[produto]
-        return None
