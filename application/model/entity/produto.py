b6ad3f1f5c0c23985955a7e47e41a4de1ab5c5d9
Pedro Quintella <pedro.q2000@outlook.com>
2020-12-23 19:32:15 -0300

    Fix: Correção da entidade produto.py
    Foi corrigida a entidade produto.py, onde foram adicionados os atributos 'valorParcela' e 'qtdParcela', e removido o atributo 'parcelamento'.

--- application/model/entity/produto.py
+++ application/model/entity/produto.py
@@ -1,15 +1,20 @@
 class Produto:
 
-    def __init__(self, id, nome, descricao, precoPadrao, precoPromocional, parcelamento):
+    def __init__(self, id, nome, fotoProduto, descricao, precoNormal, precoPromocional, valorParcela, qtdParcela):
         self._id = id
+        self._fotoProduto = fotoProduto
         self._nome = nome
         self._descricao = descricao
-        self._precoPadrao = precoPadrao
+        self._precoNormal = precoNormal
         self._precoPromocional = precoPromocional
-        self._parcelamento = parcelamento
+        self._valorParcela = valorParcela
+        self._qtdParcela = qtdParcela
     
     def get_id(self):
         return self._id
+    
+    def get_fotoProduto(self):
+        return self._fotoProduto
     
     def get_nome(self):
         return self._nome
@@ -17,12 +22,17 @@ class Produto:
     def get_descricao(self):
         return self._descricao
     
-    def get_precoPadrao(self):
-        return self._precoPadrao
+    def get_precoNormal(self):
+        return self._precoNormal
     
     def get_precoPromocional(self):
         return self._precoPromocional
     
-    def get_parcelamento(self):
-        return self._parcelamento
+    def get_valorParcela(self):
+        return self._valorParcela
+
+    def get_qtdParcela(self):
+        return self._qtdParcela
+
+    
     
